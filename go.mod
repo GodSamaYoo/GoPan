4f259ee5c2bac69366bfae49b403824051b607e9
godsamayun@gmail.com <159357zxwA>
2021-02-28 16:47:28 +0800

    first

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module GoPan
 go 1.15
 
 require (
+	github.com/GeertJohan/go.rice v1.0.2
 	github.com/labstack/echo/v4 v4.2.0
 	github.com/mattn/go-sqlite3 v1.14.6 // indirect
 	github.com/mholt/archiver/v3 v3.5.0
@@ -10,6 +11,7 @@ require (
 	github.com/tidwall/gjson v1.6.8
 	github.com/tidwall/pretty v1.0.5 // indirect
 	github.com/zyxar/argo v0.0.0-20201218032338-28f97c7a4b97
+	golang.org/x/crypto v0.0.0-20200820211705-5c72a883971a
 	gopkg.in/ini.v1 v1.62.0
 	gorm.io/driver/sqlite v1.1.4
 	gorm.io/gorm v1.20.12
--- main.go
+++ main.go
@@ -1,15 +1,20 @@
 package main
 
 import (
+	rice "github.com/GeertJohan/go.rice"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/robfig/cron/v3"
+	"golang.org/x/crypto/acme/autocert"
+	"net/http"
 	"strconv"
 )
 
 
 func main() {
 	e := echo.New()
+	e.AutoTLSManager.Cache = autocert.DirCache(".cache")
+	e.Pre(middleware.HTTPSRedirect())
 	CheckIni()
 	CheckSqlite()
 	RegisterRoutes(e)
@@ -23,8 +28,11 @@ func main() {
 	c := cron.New()
 	_, _ = c.AddFunc("*/50 * * * *", RefreshAllToken)
 	c.Start()
+	assetHandler := http.FileServer(rice.MustFindBox("html").HTTPBox())
+	e.GET("/*",echo.WrapHandler(assetHandler))
 	e.Use(middleware.CORS())
 	e.HideBanner = true
 	e.Use(middleware.GzipWithConfig(middleware.GzipConfig{Level: 3}))
 	e.Logger.Fatal(e.Start(":" + ServicePort))
+	e.StartAutoTLS(":443")
 }
