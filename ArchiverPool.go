0efdc03dbacac1b39d35b57819d8284efccaeb2e
godsamayun@gmail.com <159357zxwA>
2021-03-24 19:46:54 +0800

    aria2下载完成后多协程上传

--- ArchiverPool.go
+++ ArchiverPool.go
@@ -1,28 +1,29 @@
 package main
 
 type Tasks struct {
-	length int64
-	email ,path1,path2,path3 string
+	length                     int64
+	email, path1, path2, path3 string
 }
 
-func (t *Tasks)Execute()  {
-	FileUpOneDrive(t.length,t.email ,t.path1,t.path2,t.path3)
+func (t *Tasks) Execute() {
+	FileUpOneDrive(t.length, t.email, t.path1, t.path2, t.path3)
 }
+
 type Pool struct {
-	WorkNum int
+	WorkNum     int
 	JobsChannel chan *Tasks
-	process int
+	process     int
 }
 
-func (p *Pool)worker()  {
-	for a := range p.JobsChannel{
+func (p *Pool) worker() {
+	for a := range p.JobsChannel {
 		a.Execute()
 		p.process++
 	}
 }
 
-func (p *Pool)Run()  {
-	for i:=0;i<p.WorkNum;i++ {
+func (p *Pool) Run() {
+	for i := 0; i < p.WorkNum; i++ {
 		go p.worker()
 	}
 }
@@ -30,7 +31,7 @@ func NewPool(num int) *Pool {
 	p := Pool{
 		WorkNum:     num,
 		JobsChannel: make(chan *Tasks),
-		process: 0,
+		process:     0,
 	}
 	return &p
 }
